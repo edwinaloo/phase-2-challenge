3a159b1f3c8a86ae65dd05b21cf0724eefd6e393
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-04-27 14:26:56 +0300

    ~ push feil edit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import TransactionTable from './TransactionTable';
+import TransactionForm from './TransactionForm';
 
-function App() {
+const App = () => {
+  const [transactions, setTransactions] = useState([]);
+  const [filteredTransactions, setFilteredTransactions] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    // Fetch transactions from the server
+    fetch('http://localhost:8001/transactions')
+      .then((response) => response.json())
+      .then((data) => {
+        setTransactions(data);
+        setFilteredTransactions(data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  const handleAddTransaction = (newTransaction) => {
+    setFilteredTransactions([...filteredTransactions, newTransaction]);
+  };
+
+  const handleSearch = (e) => {
+    const searchTerm = e.target.value.toLowerCase();
+    setSearchTerm(searchTerm);
+    const filtered = transactions.filter((transaction) =>
+      transaction.description.toLowerCase().includes(searchTerm)
+    );
+    setFilteredTransactions(filtered);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        placeholder="Search by description"
+        value={searchTerm}
+        onChange={handleSearch}
+      />
+      <TransactionTable transactions={filteredTransactions} />
+      <TransactionForm onAddTransaction={handleAddTransaction} />
     </div>
   );
-}
+};
 
 export default App;
+
